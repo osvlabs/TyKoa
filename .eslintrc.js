5c4e87df6647aa846eabcdd2887d2b2fd20ad688
lgh06 <hnnk@qq.com>
2019-11-27 21:01:36 +0800

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
     "parserOptions": {
         "ecmaVersion": 2018,
         "sourceType": "module",
-        "project": "./tsconfig.json"
+        // "project": "./tsconfig.json"
     },
     "plugins": [
         "@typescript-eslint"
