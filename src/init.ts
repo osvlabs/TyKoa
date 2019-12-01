b4fdbb33083bbe7364b889ee982126f2c163e782
Happy Lau <liugehuan@gmail.com>
2019-12-01 13:45:53 +0800

    wrapped to one function

--- src/init.ts
+++ src/init.ts
@@ -5,40 +5,41 @@
 import dotenv, { DotenvParseOutput } from 'dotenv'
 import path from 'path'
 
-let overrideMode = false
-
-function loadBaseEnv(): DotenvParseOutput | undefined{
-  const result = dotenv.config()
-
-  let baseConfig = result.parsed
-  if (result.error) {
-    baseConfig = undefined
+/**
+ * load dotenv values to process.env.xxx
+ */
+function initEnv(): void{
+  let overrideMode = false;
+    function loadBaseEnv(): DotenvParseOutput | undefined {
+    const result = dotenv.config();
+      let baseConfig = result.parsed;
+      if (result.error) {
+      baseConfig = undefined;
+    }
+    if (baseConfig && baseConfig.ENV_OVERRIDE_MODE && String(baseConfig.ENV_OVERRIDE_MODE).toLowerCase() === 'true') {
+      overrideMode = true; // later env value will override both before's env value and NODE_ENV value
+    }
+    return baseConfig;
   }
-  if (baseConfig && baseConfig.ENV_OVERRIDE_MODE && String(baseConfig.ENV_OVERRIDE_MODE).toLowerCase() === 'true' ) {
-    overrideMode = true // later env value will override both before's env value and NODE_ENV value
+  function getFilePath(envir: string): string {
+    return path.resolve(process.cwd(), 'conf', `${envir}`);
   }
-  return baseConfig
-}
-
-function getFilePath(envir: string): string {
-  return path.resolve(process.cwd(), 'conf' , `${envir}`)
-}
-
-function loadEnv(envir: string): void{
-  const result = dotenv.config({
-    path: getFilePath(envir)
-  })
-  // if overrideMode is on, ignore and override existing process.env values
-  if (result && result.parsed && !result.error && overrideMode) {
-    for (const k in result.parsed) {
-      process.env[k] = result.parsed[k]
+  function loadEnv(envir: string): void {
+    const result = dotenv.config({
+      path: getFilePath(envir)
+    });
+      // if overrideMode is on, ignore and override existing process.env values
+      if (result && result.parsed && !result.error && overrideMode) {
+      for (const k in result.parsed) {
+        process.env[k] = result.parsed[k];
+      }
     }
   }
+  const baseConfig = loadBaseEnv();
+    if (baseConfig && baseConfig.ENVIR) {
+    loadEnv(baseConfig.ENVIR);
+  }
 }
 
+initEnv()
 
-const baseConfig = loadBaseEnv()
-
-if (baseConfig && baseConfig.ENVIR) {
-  loadEnv(baseConfig.ENVIR)
-}
