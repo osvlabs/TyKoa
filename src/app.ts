ac120d0d244ad5f7d59cdb031fc721fc824f3627
lgh06 <hnnk@qq.com>
2019-12-17 14:40:08 +0800

    keep app.ts clean, moved init logics to init directory

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,25 @@
 import 'reflect-metadata'
 import './init/initEnv'
-import { initKoa, initPostgraphile } from './init/index'
+import { initKoa, init } from './init/index'
 import * as routes from './routes/index'
-// import Router from '@koa/router';
-import { pg } from './db/index'
-
 
 import './decorators/index'
 
-const app = initKoa()
-initPostgraphile(app)
-
-// response
-
-// app.use(async ctx => {
-//   ctx.body = 'Hello World';
-// });
-// const api: {
-//   [key: string]: {
-//     router: Router<any, {}>
-//   }
-// } = routes
-const api: any = routes
-Object.keys(api).forEach((v) => {
-  if (api[v] && api[v].router) { // api[v] is a module alias
-    app.use(api[v].router.routes())
-  } else if (api[v] && api[v].routes && api[v].get) { // api[v] is a router / Router instance
-    app.use(api[v].routes())
-  }
-})
-
-const waitPg = async (): Promise<void> => {
-  await pg.select(pg.raw('1'))
+/* eslint-disable func-names */
+const main = async function (): Promise<void> {
+  const app = initKoa()
+  const api: any = routes
+  Object.keys(api).forEach((v) => {
+    if (api[v] && api[v].router) { // api[v] is a module alias
+      app.use(api[v].router.routes())
+    } else if (api[v] && api[v].routes && api[v].get) { // api[v] is a router / Router instance
+      app.use(api[v].routes())
+    }
+  })
+  await init(app)
   // eslint-disable-next-line no-console
-  console.log('data base connected')
-  app.listen(3001)
+  console.log(process.env.DB_PG_PORT)
+  app.listen(3000)
 }
 
-waitPg()
-
-// eslint-disable-next-line no-console
-console.log(process.env.DB_PG_PORT)
+main()
--- src/init/index.ts
+++ src/init/index.ts
@@ -1,1 +1,18 @@
+
+import Koa from 'koa'
+import { pg } from '../db/index'
+import { initPostgraphile } from './initKoa'
+
+
+async function init(app: Koa): Promise<void> {
+  initPostgraphile(app)
+  const waitPg = async (): Promise<void> => {
+    await pg.select(pg.raw('1'))
+    // eslint-disable-next-line no-console
+    console.log('data base connected')
+  }
+  await waitPg()
+}
+
+export { init, init as default }
 export * from './initKoa'
