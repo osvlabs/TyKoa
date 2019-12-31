486d0f90c362412e23cae3386d399f44dd12d7c4
lgh06 <hnnk@qq.com>
2019-12-31 17:43:49 +0800

    add comment

--- src/routes/testfolder/test2.ts
+++ src/routes/testfolder/test2.ts
@@ -9,6 +9,8 @@ import {
 
 @Controller('child')
 export default class TestController {
+  // sub folder name / file name / class name counts nothing in route path
+  // only method's name will be part of route if no route sepecified
   @Get('test/1')
   async someGetMethod(ctx: Koa.BaseContext, next: Next): Promise<void> {
     const result = await db.table('user_tb').select('*')
