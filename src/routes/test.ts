78d7ce82caf1a270389b84a086756122385bcb6b
lgh06 <hnnk@qq.com>
2019-12-31 17:47:07 +0800

    fix type

--- src/routes/test.ts
+++ src/routes/test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable import/no-named-default */
-import { Next, ParameterizedContext, default as Koa } from 'koa'
+import { Next, default as Koa } from 'koa'
 import { db } from '../db'
 
 import {
@@ -10,14 +10,14 @@ import {
 @Controller()
 export default class TestController {
   @Get('test1')
-  async someGetMethod(ctx: Koa.BaseContext, next: Next): Promise<void> {
+  async someGetMethod(ctx: Koa.Context, next: Next): Promise<void> {
     const result = await db.table('user_tb').select('*')
     ctx.body = `Hello World 1!${JSON.stringify(result)}`
     await next()
   }
 
   @Get('')
-  async someGetMethod2(ctx: ParameterizedContext<any>, next: Next): Promise<void> {
+  async someGetMethod2(ctx: Koa.Context, next: Next): Promise<void> {
     ctx.body = `Hello World 2!${JSON.stringify(ctx.header)}`
     await next()
   }
--- src/routes/testfolder/test2.ts
+++ src/routes/testfolder/test2.ts
@@ -1,6 +1,6 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable import/no-named-default */
-import { Next, ParameterizedContext, default as Koa } from 'koa'
+import { Next, default as Koa } from 'koa'
 import { db } from '../../db'
 
 import {
@@ -12,14 +12,14 @@ export default class TestController {
   // sub folder name / file name / class name counts nothing in route path
   // only method's name will be part of route if no route sepecified
   @Get('test/1')
-  async someGetMethod(ctx: Koa.BaseContext, next: Next): Promise<void> {
+  async someGetMethod(ctx: Koa.Context, next: Next): Promise<void> {
     const result = await db.table('user_tb').select('*')
     ctx.body = `Hello World Child 1!${JSON.stringify(result)}`
     await next()
   }
 
   @Get()
-  async someGetMethod2(ctx: ParameterizedContext<any>, next: Next): Promise<void> {
+  async someGetMethod2(ctx: Koa.Context, next: Next): Promise<void> {
     ctx.body = `Hello World Child 2!${JSON.stringify(ctx.header)}`
     await next()
   }
